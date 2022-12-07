eeb2e91912d68c872363b163f03240e191ecc57c
Nandhini-K-01 <85298236+Nandhini-K-01@users.noreply.github.com>
2022-12-07 16:16:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import StackOverflow from "./components/StackOverflow";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import Question from "./components/AddQuestion/Question";
+import ViewQuestion from "./components/ViewQuestion";
+import Auth from "./components/Auth";
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <BrowserRouter>
+          <Header/>
+        <Routes>
+          <Route exact path="/" element={<Auth/>}/>
+          <Route exact path="/add-question" element={<Question/>}/>
+          <Route exact path="/view-question" element={<ViewQuestion/>}/>
+          <Route exact path="/questions" element={<StackOverflow/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
